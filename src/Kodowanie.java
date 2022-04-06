9ec4723d49d84d507cc61f4fe41668d2a60eb0cb
LachTomasz <lach.tomasz@gmail.com>
2022-04-06 23:59:58 +0200

    new

--- src/Kodowanie.java
+++ src/Kodowanie.java
@@ -1,7 +1,11 @@
+import java.nio.charset.StandardCharsets;
+
 public class Kodowanie {
     public static void main(String[] args) {
         String arg = args[0];
         System.out.print("String: "+arg+"\tpo zamianie na ciąg odpowiadających bajtów: ");
+        byte[] kod = arg.getBytes(StandardCharsets.UTF_8);
+
         System.out.println(String.valueOf(arg.toCharArray()));
     }
 }
