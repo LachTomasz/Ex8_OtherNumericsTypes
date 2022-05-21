2934d21eabb95cdecb74213246ab12e97ec36125
LachTomasz <lach.tomasz@gmail.com>
2022-05-21 21:23:40 +0200

    finally

---
+++ src/Txt2Hex.java
@@ -0,0 +1,10 @@
+import com.sun.jdi.ByteValue;
+
+public class Txt2Hex {
+    public static void main(String[] args) {
+        String text = "Szyfr";
+        byte[] textByte = text.getBytes();
+        for(byte b : textByte) System.out.print(Integer.toHexString(b).toUpperCase()+" ");
+
+    }
+}
